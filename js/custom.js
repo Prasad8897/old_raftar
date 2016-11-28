1ceefa2488ff23f09e9886628f5030f58e39d043
Karthik Nishanth <nishanthkarthik@live.com>
2016-11-28 15:02:44 +0530

    bugfix

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+var terms = ["highly determined", "enthusiastic", "active", "innovative", "creative", "dedicated", "passionate"];
+
+function cycleTerm() {
+  var ct = $("#cyclic-text").data("term") || 0;
+  $("#cyclic-text").data("term", ct == terms.length -1 ? 0 : ct + 1).text(terms[ct]).fadeIn()
+              .delay(1500).fadeOut(200, cycleTerm);
+}
+$(cycleTerm);
